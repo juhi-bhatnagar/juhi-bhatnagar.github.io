7c09dfc3c9ed87195462b83b9c7f6387feae6b93
unknown <juhi.bhatnagar95@gmail.com>
2024-05-28 22:35:07 -0700

    add website base

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="AppRoot">
+      <header className="MenuBar"></header>
+      <h1 className="Name">Juhi Bhatnagar</h1>
+      <h2 className="Title">About me</h2>
+
+      <h2 className="Title">Professional experience</h2>
+      <h3 className="Subtitle">Microsoft</h3>
+      <h3 className="Subtitle">Schneider Electric</h3>
+      <h3 className="Subtitle">Precog Research Group</h3>
+
+      <h2 className="Title">Education</h2>
+      <h3 className="Subtitle">Rice University</h3>
+      <h3 className="Subtitle">Indraprastha Institute of Information Technology</h3>
+
+      <h2 className="Title">Leadership</h2>
+      <h3 className="Subtitle">ODSP GLEAM</h3>
+      <h3 className="Subtitle">ODSP Mentoring</h3>
+      <h3 className="Subtitle">TEDxRiceU Salon Speaker</h3>
+      <h3 className="Subtitle">MAGIC Mentor</h3>
+      <h3 className="Subtitle">Intern Ambassador</h3>
+      <h3 className="Subtitle">Editor-in-Chief</h3>
+
+      <h2 className="Title">Awards</h2>
+      <h3 className="Subtitle">Google Women Techmakers Scholarship</h3>
+
+      <h2 className="Title">Hobbies</h2>
+      <h3 className="Subtitle">Languages</h3>
     </div>
   );
 }
